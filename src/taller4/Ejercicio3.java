eac12de1f133b7af8341a44c83adf4439857d3a3
AntonioAguilar99 <antonio.aguilarvelandia@unicolombo.edu.co>
2025-04-21 08:05:12 -0500

    refactor: se creó una nueca rama llamada Taller4 para organizar mejor el historial de cambios del proyecto, por recomendación del profesor

--- src/taller4/Ejercicio3.java
+++ src/taller4/Ejercicio3.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package taller4;
 import javax.swing.*;
 import java.awt.event.*;
 /**
  *
- * @author anton
+ * @author Andonio Aguilar
  */
 public class Ejercicio3 extends JFrame {
     JTextField txtFrase, txtPalabra1, txtPalabra2, txtPalabra3, txtPalabra4;
